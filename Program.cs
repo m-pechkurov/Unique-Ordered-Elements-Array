c95a67b2cb0034792c80b5a4f65dc56796111e9b
Matvey Pechkurov <m.pechkurov@sh.dunice.net>
2024-04-18 17:27:13 +0300

    feature/added solution

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using WorkArea;
+
+// Пример использования на строке
+var example1 = UniqueOrderedElementsArray.DoIt("aaaaaaaaaaasdasdasdasdasdazzzzzzq");
+Console.WriteLine("Пример 1 (строка):");
+PrintCollection(example1);
+
+// Пример использования на массиве целых чисел
+var example2 = UniqueOrderedElementsArray.DoIt(new[] { 1, 2, 38, 1, 1, 1, 2, 2, 2, 42, 16, 16 });
+Console.WriteLine("\nПример 2 (массив целых чисел):");
+PrintCollection(example2);
+
+// Пример использования на очереди
+Queue<int> queue = new Queue<int>();
+queue.Enqueue(1);
+queue.Enqueue(2);
+queue.Enqueue(2);
+queue.Enqueue(3);
+queue.Enqueue(3);
+var example4 = UniqueOrderedElementsArray.DoIt(queue);
+Console.WriteLine("\nПример 4 (очередь):");
+PrintCollection(example4);
+
+// Пример использования на списке
+List<double> list = new List<double> { 1.1, 2.2, 3.3, 3.3, 4.4, 4.4 };
+var example5 = UniqueOrderedElementsArray.DoIt(list);
+Console.WriteLine("\nПример 5 (список):");
+PrintCollection(example5);
+
+Console.ReadLine();
+
+
+// Метод для вывода элементов коллекции
+static void PrintCollection<T>(IEnumerable<T> collection)
+{
+    foreach (var item in collection)
+    {
+        Console.Write(item);
+    }
+}
---
+++ UniqueOrderedElementsArray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace WorkArea 
+{
+    public class UniqueOrderedElementsArray
+    {
+        public static List<T> DoIt<T>(IEnumerable<T> collection)
+        {
+            List<T> uniqueElements = new List<T>();
+            T prevElem = default(T);
+            var enumerator = collection.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var currentItem = enumerator.Current;
+                if (EqualityComparer<T>.Default.Equals(prevElem, currentItem))
+                {
+                    continue;
+                }
+                uniqueElements.Add(currentItem);
+                prevElem = currentItem;
+            }
+            return uniqueElements;
+        }
+    }
+}
