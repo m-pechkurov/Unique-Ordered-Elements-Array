b22acad5b28539099d3cbac6e720a5b9b3d6a4fc
Matvey Pechkurov <m.pechkurov@sh.dunice.net>
2024-04-23 16:15:34 +0300

    fix: renamed the method and added an iterator

--- UniqueOrderedElementsArray.cs
+++ UniqueOrderedElementsArray.cs
@@ -1,24 +1,25 @@
 ﻿using System;
-namespace WorkArea 
+namespace WorkArea
 {
     public class UniqueOrderedElementsArray
     {
-        public static List<T> DoIt<T>(IEnumerable<T> collection)
+        public static IEnumerable<T> GetUniqueOrderedElements<T>(IEnumerable<T> collection)
         {
-            List<T> uniqueElements = new List<T>();
             T prevElem = default(T);
             var enumerator = collection.GetEnumerator();
+
             while (enumerator.MoveNext())
             {
                 var currentItem = enumerator.Current;
+
                 if (EqualityComparer<T>.Default.Equals(prevElem, currentItem))
                 {
                     continue;
                 }
-                uniqueElements.Add(currentItem);
+
+                yield return currentItem;
                 prevElem = currentItem;
             }
-            return uniqueElements;
         }
     }
 }
